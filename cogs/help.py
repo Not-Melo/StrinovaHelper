0c8fb58dc31e5095c3dd6ac42310c0da23cdfb15
Not-Melo <2024ben.johnson@gmail.com>
2024-06-30 14:43:03 -0500

    removed a cmd in help command

--- cogs/help.py
+++ cogs/help.py
@@ -16,11 +16,6 @@ class InfoSystem(commands.Cog):
     @app_commands.command(name="help", description="helpful command to teach you about the bot.")
     async def help_cmd(self, interaction: discord.Interaction):
         embed = discord.Embed(title="🆘 | Helpful command", color=0xFFFFFF)
-        embed.add_field(
-            name="/make_profile", 
-            value="This command is not yet ready for use...",
-            inline=True
-            )
         embed.set_footer(text="Maintained by the community.")
         await interaction.response.send_message(embed=embed, ephemeral=True)
 
